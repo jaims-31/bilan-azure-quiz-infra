8f1c30dc1f71d7dc9685993876deac562a2f1ec8
Franck Barry <jaimsmuzik@gmail.com>
2026-08-25 21:54:40 +0200

    chore: ignore macOS and VS Code workspace files

---
+++ providers.tf
@@ -0,0 +1,21 @@
+terraform {
+  required_version = ">= 1.9"
+
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 4.0"
+    }
+  }
+
+  backend "azurerm" {
+    resource_group_name  = "fbarryRG"
+    storage_account_name = "stfbarrytfstate"
+    container_name        = "tfstate"
+    key                   = "bilan-azure-quiz.tfstate"
+  }
+}
+
+provider "azurerm" {
+  features {}
+}
