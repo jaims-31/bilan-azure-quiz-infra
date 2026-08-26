bf50bba464428615646ecf3a0e97cf42f1e2bfca
Franck Barry <jaimsmuzik@gmail.com>
2026-08-26 09:28:22 +0200

    feat: application storage account with Azure AD authentication

--- role_assignments.tf
+++ role_assignments.tf
@@ -3,3 +3,9 @@ resource "azurerm_role_assignment" "current_user_kv_secrets_officer" {
   role_definition_name = "Key Vault Secrets Officer"
   principal_id          = data.azurerm_client_config.current.object_id
 }
+
+resource "azurerm_role_assignment" "current_user_storage_blob_contributor" {
+  scope                = azurerm_storage_account.main.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id          = data.azurerm_client_config.current.object_id
+}
---
+++ storage_account.tf
@@ -0,0 +1,21 @@
+resource "azurerm_storage_account" "main" {
+  name                       = "stfbarryquizapp"
+  resource_group_name        = data.azurerm_resource_group.main.name
+  location                   = data.azurerm_resource_group.main.location
+  account_tier                = "Standard"
+  account_replication_type    = "LRS"
+  min_tls_version              = "TLS1_2"
+  shared_access_key_enabled   = false
+
+  tags = merge(local.common_tags, {
+    composant = "storage"
+  })
+}
+
+resource "azurerm_storage_container" "quiz_media" {
+  name                   = "quiz-media"
+  storage_account_id     = azurerm_storage_account.main.id
+  container_access_type  = "private"
+
+  depends_on = [azurerm_role_assignment.current_user_storage_blob_contributor]
+}
