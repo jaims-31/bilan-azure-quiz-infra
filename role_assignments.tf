f4877dc6c5dfc7ddde825282b4c3a76e4f9e71a0
Franck Barry <jaimsmuzik@gmail.com>
2026-08-26 11:14:15 +0200

    feat: grant backend Web App identity access to Key Vault and Storage

--- role_assignments.tf
+++ role_assignments.tf
@@ -9,3 +9,15 @@ resource "azurerm_role_assignment" "current_user_storage_blob_contributor" {
   role_definition_name = "Storage Blob Data Contributor"
   principal_id          = data.azurerm_client_config.current.object_id
 }
+
+resource "azurerm_role_assignment" "backend_kv_secrets_user" {
+  scope                = azurerm_key_vault.main.id
+  role_definition_name = "Key Vault Secrets User"
+  principal_id          = azurerm_linux_web_app.backend.identity[0].principal_id
+}
+
+resource "azurerm_role_assignment" "backend_storage_blob_contributor" {
+  scope                = azurerm_storage_account.main.id
+  role_definition_name = "Storage Blob Data Contributor"
+  principal_id          = azurerm_linux_web_app.backend.identity[0].principal_id
+}
