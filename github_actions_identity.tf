8d839a56af7e81d64cbc20351e8f3ec9e3518799
Franck Barry <jaimsmuzik@gmail.com>
2026-08-26 11:37:05 +0200

    feat: create GitHub Actions identity with OIDC federation for infra repo

---
+++ github_actions_identity.tf
@@ -0,0 +1,23 @@
+resource "azurerm_user_assigned_identity" "github_actions" {
+  name                = "id-github-actions-fbarry"
+  resource_group_name = data.azurerm_resource_group.main.name
+  location            = data.azurerm_resource_group.main.location
+
+  tags = merge(local.common_tags, {
+    composant = "cicd"
+  })
+}
+
+resource "azurerm_federated_identity_credential" "infra_main_branch" {
+  name                = "github-infra-main"
+    user_assigned_identity_id = azurerm_user_assigned_identity.github_actions.id
+  audience            = ["api://AzureADTokenExchange"]
+  issuer              = "https://token.actions.githubusercontent.com"
+  subject             = "repo:jaims-31/bilan-azure-quiz-infra:ref:refs/heads/main"
+}
+
+resource "azurerm_role_assignment" "github_actions_contributor" {
+  scope                = data.azurerm_resource_group.main.id
+  role_definition_name = "Contributor"
+  principal_id          = azurerm_user_assigned_identity.github_actions.principal_id
+}
