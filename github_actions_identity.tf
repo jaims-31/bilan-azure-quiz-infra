8cfc4c88c26af0523e43aea77b4f4d4d8fd98083
Franck Barry <jaimsmuzik@gmail.com>
2026-08-27 11:11:09 +0200

    feat: add OIDC federation for backend and frontend repos

--- github_actions_identity.tf
+++ github_actions_identity.tf
@@ -28,3 +28,19 @@ resource "azurerm_role_assignment" "github_actions_kv_secrets_user" {
   role_definition_name = "Key Vault Secrets User"
   principal_id         = azurerm_user_assigned_identity.github_actions.principal_id
 }
+
+resource "azurerm_federated_identity_credential" "backend_main_branch" {
+  name                      = "github-backend-main"
+  user_assigned_identity_id = azurerm_user_assigned_identity.github_actions.id
+  audience                  = ["api://AzureADTokenExchange"]
+  issuer                    = "https://token.actions.githubusercontent.com"
+  subject                   = "repo:jaims-31@172600556/bilan-azure-quiz-backend@1342028922:ref:refs/heads/main"
+}
+
+resource "azurerm_federated_identity_credential" "frontend_main_branch" {
+  name                      = "github-frontend-main"
+  user_assigned_identity_id = azurerm_user_assigned_identity.github_actions.id
+  audience                  = ["api://AzureADTokenExchange"]
+  issuer                    = "https://token.actions.githubusercontent.com"
+  subject                   = "repo:jaims-31@172600556/bilan-azure-quiz-frontend@1342030958:ref:refs/heads/main"
+}
