9cdda7d98fbed79863a34bcb57190eaf6aa492e3
Franck Barry <jaimsmuzik@gmail.com>
2026-08-27 10:30:27 +0200

    style: apply terraform fmt formatting

--- github_actions_identity.tf
+++ github_actions_identity.tf
@@ -26,5 +26,5 @@ resource "azurerm_role_assignment" "github_actions_contributor" {
 resource "azurerm_role_assignment" "github_actions_kv_secrets_user" {
   scope                = azurerm_key_vault.main.id
   role_definition_name = "Key Vault Secrets User"
-  principal_id          = azurerm_user_assigned_identity.github_actions.principal_id
+  principal_id         = azurerm_user_assigned_identity.github_actions.principal_id
 }
